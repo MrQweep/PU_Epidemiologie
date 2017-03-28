2f961509ee3ae621379a26c146e1d92afbc1c4a1
Nico Berling <nicob99@gmx.ch>
2017-03-28 10:41:34 +0200

    Adjusted equation, moved inputs into constructor

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
 import models
 
-SIR_object = models.SIR()
+SIR_object = models.SIR(60,0.1,499,1,0,0.001,0.1)
+
--- models.py
+++ models.py
@@ -1,35 +1,36 @@
 import matplotlib.pyplot as plt
 
-
 class SIR:
-    def __init__(self):
-        self.max_time = int(input("Time: "))
-        self.n=int(input("Population: "))
-        self.s=float(input("Percentage of Susceptible: "))
-        self.i=float(input("Percentage of Infected: "))
-        self.r=float(input("Percentage of Recovered: "))
-        self.beta=float(input("Beta: "))
-        self.gamma=float(input("Gamma: "))
+    def __init__(self,max_time=int(input("Time: ")), dt = int(input("Timestep: ")),s=float(input("Susceptible: ")),i=float(input("Infected: ")),r=float(input("Recovered: ")),beta=float(input("Beta: ")),gamma=float(input("Gamma: "))):
+        self.max_time = max_time
+        self.dt = dt
+        self.s=s
+        self.i=i
+        self.r=r
+        self.beta=beta
+        self.gamma=gamma
         self.t = 0
         self.s_record = []
         self.i_record = []
         self.r_record = []
+        self.t_record = []
         while self.t < self.max_time:
-            self.t += 1
-            self.timestep()
+            self.t += self.dt
+            self.timestep(self.dt)
             print(str(round(self.s + self.i + self.r,4)) + " : " + str(self.s) + " / " + str(self.i) + " / " + str(self.r))
-            self.s_record.append(self.s * self.n)
-            self.i_record.append(self.i * self.n)
-            self.r_record.append(self.r * self.n)
-        plt.plot(self.s_record, 'g')
-        plt.plot(self.i_record, 'r')
-        plt.plot(self.r_record, 'b')
+            self.s_record.append(self.s)
+            self.i_record.append(self.i)
+            self.r_record.append(self.r)
+            self.t_record.append(self.t)
+        plt.plot(self.t_record, self.s_record, 'g')
+        plt.plot(self.t_record, self.i_record, 'r')
+        plt.plot(self.t_record, self.r_record, 'b')
         plt.show()
 
-    def timestep(self):
+    def timestep(self,dt):
         ds = -(self.beta * self.i * self.s)
         di = (self.beta * self.i * self.s) - self.gamma * self.i
         dr = self.gamma * self.i
-        self.s += ds
-        self.i += di
-        self.r += dr
+        self.s += ds*dt
+        self.i += di*dt
+        self.r += dr*dt
